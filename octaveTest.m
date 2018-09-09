762e21281c57912dc3839d9cc3dbbcb85372a87f
Timo Rantalainen <tjrantal@gmail.com>
2018-09-09 10:23:41 +0300

    Updated to use jogl 2.3.2 and gradle to build

--- octaveTest.m
+++ octaveTest.m
@@ -1,16 +1,7 @@
-%setenv("JAVA_HOME","D:/timo/compiler/jdk6u31/")
-cd 'D:/timo/programming/java/jogl/visualizeAxes';
-if 0
-	javaaddpath('jarLib/gluegen.jar');
-	javaaddpath('jarLib/gluegen-natives-windows-amd64.jar');
-	javaaddpath('jarLib/jogl-all.jar');
-	javaaddpath('jarLib/jogl-all-natives-windows-amd64.jar');
-	javaaddpath('build/VisualizeAxes.jar');
-	javaaddpath('lib');
-	javaaddpath('lib32');
-	javaaddpath('build/java');
-	javaaddpath('.');
-end
+close all;
+fclose all;
+clc;
+
 rotation = [1:600]/180*pi;
 tempQuatArr = single([cos(rotation/2);sin(rotation/2)*0;sin(rotation/2)*0;sin(rotation/2)*1]);
 fh = fopen('quaternions.tab','wb','ieee-be');
