2e843b451f0daad1eb788e37071be4b72501efed
Timo Rantalainen <tjrantal@gmail.com>
2018-09-09 10:33:22 +0300

    Fixed things to work with jogl 2.3.2 and gradle

--- octaveTest.m
+++ octaveTest.m
@@ -2,10 +2,14 @@ close all;
 fclose all;
 clc;
 
-rotation = [1:600]/180*pi;
-tempQuatArr = single([cos(rotation/2);sin(rotation/2)*0;sin(rotation/2)*0;sin(rotation/2)*1]);
+fps = 30;
+t = ([1:10*fps]-1)/fps;
+rotation = 2*pi*t;
+tempQuatArr = double([cos(rotation/2);sin(rotation/2)*0;sin(rotation/2)*0;sin(rotation/2)*1]');
+size(tempQuatArr)
+%keyboard;
 fh = fopen('quaternions.tab','wb','ieee-be');
-fwrite(fh,tempQuatArr,'single');
+fwrite(fh,tempQuatArr,'double');
 fclose(fh);
 
 %testVisualizeAxes = javaObject('timo.test.VisualizeAxes');%,int32(800),int32(500),quatArr);
