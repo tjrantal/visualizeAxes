bd944407478a6ef365e06f05ff7383eb9bf30934
Timo Rantalainen <tjrantal@gmail.com>
2018-09-10 17:04:32 +0300

    Added visualiser for MIMU orientation. To be used with Matlab or Octave

---
+++ testMIMUOrientationVisualisation.m
@@ -0,0 +1,17 @@
+fclose all;
+close all;
+clear all;
+clc;
+
+javaaddpath('build/libs/visualizeAxes-1.0.jar');
+
+mimuOV = javaObject('timo.test.MIMUOrientationVisualisator',600,600,500,200);
+esa = 1;
+
+for i = 1:360
+   theta = i/180*pi; 
+   mimuOV.setRotationQuaternion([cos(theta/2),0,sin(theta/2),0])
+   pause(1/60)
+end
+
+mimuOV.shutdown();
